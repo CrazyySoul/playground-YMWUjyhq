b9f7e0c783a04d892fa19652070fa14e8922ad72
Gautier DUFOUR <gautier.dufour@etu.unilim.fr>
2017-10-30 14:59:52 +0100

    Update DessertTest.java

--- java-project/src/test/DessertTest.java
+++ java-project/src/test/DessertTest.java
@@ -12,23 +12,7 @@ public class DessertTest {
 		Dessert d1 = new Gaufre();
 	    d1 = new Chocolat(d1);
 	    
-		assertEquals("Gaufre, chocolat : 2,00€", d1.toString());
+		Assert.assertEquals("Gaufre, chocolat : 2,00€", d1.toString());
 	}
 	
-	@Test
-	public void test_crepe_chocolat_chantilly(){
-		Dessert d2 = new Crepe();
-	    d2 = new Chocolat(d2);
-	    d2 = new Chantilly(d2);
-	    
-	    assertEquals("Crêpe, chocolat, chantilly : 2,20€", d2.toString());
-	}
-	
-	@Test
-	public void test_crepe_chantilly(){
-	    Dessert d3 = new Crepe();
-	    d3 = new Chantilly(d3);
-	    
-	    assertEquals("Crêpe, chantilly : 2,00€", d3.toString());
-	}
 }
